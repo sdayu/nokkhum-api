b01e1249b3b0b0a8769a6ce26ef89902f54f2698
alpha747 <ww@braille>
2012-12-27 22:58:29 +0700

    indent corrected

--- nokkhumapi/views/operation.py
+++ nokkhumapi/views/operation.py
@@ -14,51 +14,52 @@ from nokkhumapi import models
 class Operating(object):
     def __init__(self, request):
         self.request = request
-    def operating(request):
-    matchdict   = request.matchdict
-    camera_name = matchdict['name']
-    operating   = matchdict['operating']
-
-    camera = models.Camera.objects(owner=request.user, name=camera_name).first()
+        
+    def operating(self):
+        matchdict   = request.matchdict
+        camera_name = matchdict['name']
+        operating   = matchdict['operating']
     
-    if not camera:
-        return Response('Camera not found')
+        camera = models.Camera.objects(owner=request.user, name=camera_name).first()
+        
+        if not camera:
+            return Response('Camera not found')
+        
+        command_action  = 'No-command'
+        user_command    = 'Undefine'
+        if operating == 'start':
+            command_action = 'Start'
+            user_command = 'Run'
+        elif operating == 'stop':
+            command_action = 'Stop'
+            user_command = 'Suspend'
+        
+        ccq = models.CameraCommandQueue.objects(owner=request.user, camera=camera, action=command_action).first()
+        if ccq is not None:
+            return Response('Camera name %s on operation' % camera.name)
+        
+        
+        camera.operating.status = command_action
+        camera.operating.user_command = user_command
+        camera.update_date = datetime.datetime.now()
+        camera.save()
+        
+        ccq         = models.CameraCommandQueue()
+        ccq.command_date = datetime.datetime.now()
+        ccq.update_date = datetime.datetime.now()
+        ccq.action  = command_action
+        ccq.status  = 'Waiting'
+        ccq.camera  = camera
+        ccq.owner   = request.user
+        ccq.save()
     
-    command_action  = 'No-command'
-    user_command    = 'Undefine'
-    if operating == 'start':
-        command_action = 'Start'
-        user_command = 'Run'
-    elif operating == 'stop':
-        command_action = 'Stop'
-        user_command = 'Suspend'
-    
-    ccq = models.CameraCommandQueue.objects(owner=request.user, camera=camera, action=command_action).first()
-    if ccq is not None:
-        return Response('Camera name %s on operation' % camera.name)
+        return HTTPFound(location=request.route_path('project_index', name=camera.project.name))
     
     
-    camera.operating.status = command_action
-    camera.operating.user_command = user_command
-    camera.update_date = datetime.datetime.now()
-    camera.save()
-    
-    ccq         = models.CameraCommandQueue()
-    ccq.command_date = datetime.datetime.now()
-    ccq.update_date = datetime.datetime.now()
-    ccq.action  = command_action
-    ccq.status  = 'Waiting'
-    ccq.camera  = camera
-    ccq.owner   = request.user
-    ccq.save()
-
-    return HTTPFound(location=request.route_path('project_index', name=camera.project.name))
-
-
+            
         
     
-
-
-
-
-
+    
+    
+    
+    
