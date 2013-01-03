d0b4640ea5f196b5b5568930b28cf6271010e6b0
alpha747 <ww@braille>
2013-01-03 18:51:38 +0700

    add new route

--- nokkhumapi/routing.py
+++ nokkhumapi/routing.py
@@ -10,7 +10,7 @@ def add_routes(config):
     #authentication
     config.add_route('authentications.tokens', '/authentication/tokens')
     
-    
+    config.add_route('projects.cameraproject', '/cameras/{camera_id}/project')
     config.add_route('projects.userprojects', '/users/{user_id}/projects')
     # camera
     config.add_route('cameras', '/cameras*extension')
@@ -19,7 +19,8 @@ def add_routes(config):
     config.add_route('accounts', '/accounts*extension')
     config.add_route('camera_models', '/camera_models*extension')
     config.add_route('manufactories','/manufactories')
-    config.add_route('camera.camera_operating', '/camera/{camera_id}/operating')
+    config.add_route('cameras.operating', '/camera/{camera_id}/operating')
+    config.add_route('cameras.image_processor', '/camera/image_processor')
 #    config.add_route('cameras_post', '/cameras')
 #    config.add_route('cameras_get', '/cameras/{id}')
 #    config.add_route('cameras_delete', '/cameras/{id}')
