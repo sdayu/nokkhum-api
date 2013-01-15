91fd2f598d086fdf7d7aca36c8884208ea3e2522
Wongpiti  Wangsanti <w.whangsanti@gmail.com>
2013-01-15 21:05:23 +0700

    fix url sequence wrong

--- nokkhumapi/routing.py
+++ nokkhumapi/routing.py
@@ -12,16 +12,18 @@ def add_routes(config):
     
     config.add_route('projects.cameras', '/projects/{project_id}/cameras')
     config.add_route('projects.userprojects', '/users/{user_id}/projects')
-    # camera
-    config.add_route('cameras', '/cameras*extension')
+        
     config.add_route('users', '/users*extension')
     config.add_route('projects', '/projects*extension')
     config.add_route('accounts', '/accounts*extension')
     config.add_route('camera_models', '/camera_models*extension')
     config.add_route('manufactories','/manufactories')
+    
+    #cameras
     config.add_route('cameras.operating', '/cameras/{camera_id}/operating')
     config.add_route('cameras.image_processor', '/cameras/{camera_id}/processors')
     config.add_route('cameras.status', '/cameras/{camera_id}/status')
+    config.add_route('cameras', '/cameras*extension')
     
     #admin
     config.add_route('admin.users.list_users', '/admin/users')
