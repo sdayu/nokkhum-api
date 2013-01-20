db47836bd7cc7479975e40b3a2156066d9808c77
Thanathip Limna <boatkrap@gmail.com>
2013-01-20 14:06:49 +0700

    fix dlink url

--- nokkhumapi/driver/camera/dlink/base_driver.py
+++ nokkhumapi/driver/camera/dlink/base_driver.py
@@ -8,8 +8,9 @@ from ..interface import CameraDriver
 class DLinkCamera(CameraDriver):
     def __init__(self, host, username, password, port=80):
         super().__init__(host, username, password, port)
-        self.video_pattern = '/video/video.cgi'
-        self.image_pattern = '/image.jpg'
+        self.video_pattern = '/video/mjpg.cgi'
+        self.image_pattern = '/image/jpg.cgi'
+        self.audio_pattern = '/audio.cgi'
     
     def get_video_url(self):
         url = super().get_base_url()+self.video_pattern
@@ -17,4 +18,8 @@ class DLinkCamera(CameraDriver):
     
     def get_image_url(self):
         url = super().get_base_url()+self.image_pattern
+        return url
+    
+    def get_audio_url(self):
+        url = super().get_base_url()+self.audio_pattern
         return url
