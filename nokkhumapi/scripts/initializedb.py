b173928b63995dc3c2b9b0b26be16bb191664941
Thanathip Limna <boatkrap@gmail.com>
2013-02-09 15:24:36 +0700

    edit initial script for new camera model

--- nokkhumapi/scripts/initializedb.py
+++ nokkhumapi/scripts/initializedb.py
@@ -33,14 +33,15 @@ def main(argv=sys.argv):
     default_roles = ['admin', 'user']
     
     print ("begin to initial database")
-    
+    print ("initial default groups")
     for gname in default_roles:
         group = models.Role.objects(name=gname).first()
         if not group:
             group = models.Role()
             group.name = gname
             group.save()
-        
+    
+    print ("initial admin user") 
     user = models.User.objects(email='admin@nokkhum.local').first()
     if not user:
         user = models.User()
@@ -56,27 +57,81 @@ def main(argv=sys.argv):
         
     user = models.User.objects(email='admin@nokkhum.local').first()
     
-    print("test mongo: ",user.roles[0].name)
+    print ("initial default manufatories and camera models")
+    manufactories = {
+                     'Generic': ['OpenCV'],
+                     'D-Link':['DCS-930L']
+                     }
+   
+    for manu_name in manufactories:
         
-    man_count = models.Manufactory.objects().count()
-    if man_count == 0:
-        man = models.Manufactory()
-        man.name = 'Generic'
-        man.save()
+        man = models.Manufactory.objects(name=manu_name).first()
+        if man is None:
+            man = models.Manufactory()
+            man.name = manu_name
+            man.save()
         
-        camera_model = models.CameraModel()
-        camera_model.name = 'OpenCV'
-        camera_model.manufactory = man
-        camera_model.save()
+        for cam_model_name in manufactories[manu_name]:
+            camera_model = models.CameraModel.objects(name=cam_model_name, manufactory=man).first()
+            if camera_model is None:
+                camera_model = models.CameraModel()
+                camera_model.name = cam_model_name
+                camera_model.manufactory = man
+                camera_model.save()
+    
+    print ("initial default image processors")
+    processor_attributes = {
+                            'Motion Detector':
+                                {
+                                    "motion_analysis_method" : "Optical Flow",
+                                    "wait_motion_time" : 10,
+                                    "name" : "Motion Detector",
+                                    "interval" : 3,
+                                    "region_of_interest" : "",
+                                    "sensitive" : 90,
+                                    "resolution" : 98,
+                                    "enable_area_of_interest" : False,
+                                    "processors" : [ ]
+                                }
+                                , 
+                            'Face Detector':
+                                {
+                                    "interval" : 5,
+                                    "name" : "Face Detector",
+                                    "processors" : [ ]
+                                }, 
+                            'Video Recorder':
+                                {
+                                    "width" : 640,
+                                    "record_motion" : False,
+                                    "name" : "Video Recorder",
+                                    "fps" : 10,
+                                    "height" : 480
+                                }, 
+                            'Image Recorder':
+                                {
+                                    "width" : 640,
+                                    "interval" : 1,
+                                    "name" : "Image Recorder",
+                                    "height" : 480
+                                },
+                            'Multimedia Recorder':
+                                {
+                                    "width" : 640,
+                                    "name" : "Multimedia Recorder",
+                                    "fps" : 10,
+                                    "height" : 480
+                                },
+                            }
+    
+    for processor_name in processor_attributes:
+        image_processor = models.ImageProcessor.objects(name=processor_name).first()
         
-    processor_count = models.ImageProcessor.objects().count()
-    if processor_count == 0:
-        processor_name = ['Motion Detector', 'Face Detector', 
-                     'Video Recorder', 'Image Recorder']
-        
-        for name in processor_name:
-            pro = models.ImageProcessor()
-            pro.name = name
-            pro.save()
+        if image_processor is None:
+            image_processor = models.ImageProcessor()
+            image_processor.name = processor_name
+            
+        image_processor.default_attributes = processor_attributes[processor_name]
+        image_processor.save()
             
     print ("end initial database")
