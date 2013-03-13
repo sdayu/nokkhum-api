2b047e19516fcb6bd23f438f55b7e66ce7e4c8e4
Thanathip Limna <boatkrap@gmail.com>
2013-03-13 22:08:30 +0700

    add new admin module

--- nokkhumapi/views/admin/camera_running_fail.py
+++ nokkhumapi/views/admin/camera_running_fail.py
@@ -7,6 +7,8 @@ from pyramid.httpexceptions import HTTPFound
 from pyramid.view import view_config, view_defaults
 from pyramid.response import Response
 
+from nokkhumapi import models
+
 @view_defaults(route_name='admin.camera_running_fail', permission='r:admin', renderer='json')
 class CameraRunningFail:
     def __init__(self, request):
@@ -14,8 +16,22 @@ class CameraRunningFail:
         
     @view_config(route_name='admin.camera_running_fail.list', request_method="GET")
     def list_camera(self):
-        fail_status = models.CameraRunningFail.objects("-id").limit("30").all()
+        fail_status = models.CameraRunningFail.objects("-id").limit(30).all()
         return dict(
-                    run_fail_status=[]
+                    camera_running_fail=[
+                                     dict(
+                                          id=fs.id,
+                                          camera=dict(
+                                                      id=fs.camera.id
+                                                      ),
+                                          compute_node=dict(
+                                                            id=fs.compute_node.id
+                                                            ),
+                                          report_time=fs.report_time,
+                                          process_time=fs.process_time,
+                                          message=fs.message
+                                          )
+                                     for fs in fail_status
+                                     ]
                     )
         
