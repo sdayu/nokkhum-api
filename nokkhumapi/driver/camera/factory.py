43255f37492528430823424ae665a63dbc04bf9b
Thanathip Limna <boatkrap@gmail.com>
2013-09-05 12:30:20 +0700

    add new camera driver

--- nokkhumapi/driver/camera/factory.py
+++ nokkhumapi/driver/camera/factory.py
@@ -4,10 +4,14 @@ Created on Jan 18, 2013
 @author: boatkrap
 '''
 from .dlink import driver as dlink_driver
+from .axis import driver as axis_driver
 
 class CameraDriverFactory:
     def get_camera_driver(self, manufactory):
         if manufactory == 'D-Link':
             return dlink_driver.DLinkDriverFactory()
+        elif manufactory == 'Axis':
+            return axis_driver.AxisDriverFactory()
+            
         
     
