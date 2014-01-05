f80c2e3bf8af16ccea91719b6c59aac8e90f06ca
ys <yoschanin.s@gmail.com>
2014-01-05 22:19:48 +0700

    g

--- nokkhumapi/views/groups/processors.py
+++ nokkhumapi/views/groups/processors.py
@@ -22,7 +22,7 @@ class GroupCollaborator:
         if group is None:
             self.request.response.status = '404 Not Found'
             return {'message':"Group id %d not found"%project_id}
-        collaborate_projects = models.Project.objects(gcollaborators__in=group).all()
+        collaborate_projects = models.Project.objects().all()
         print(collaborate_projects)
         result = dict(
                       group=dict(id=group.id, name=group.name, collaborators=[]),
