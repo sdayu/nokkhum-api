c54dcdeddc341fc489e5371bb422627895f7ddd7
ys <yoschanin.s@gmail.com>
2014-01-18 22:41:57 +0700

    update

--- nokkhumapi/views/facetraining.py
+++ nokkhumapi/views/facetraining.py
@@ -18,7 +18,44 @@ from nokkhumapi import models
 class Facetraining(object):
     def __init__(self, request):
         self.request = request
-        
+    
+    def build_result(self, processor):
+        processor_operating=dict(
+                             user_command=processor.operating.user_command,
+                             status=processor.operating.status,
+                             update_date=processor.operating.update_date,
+                             )
+        if processor.operating.compute_node:
+            processor_operating['compute_node']=dict(
+                                               id=processor.operating.compute_node.id
+                                               )
+            try:
+                processor_operating['compute_node']['name']=processor.operating.compute_node.name
+            except:
+                pass
+            
+        result = dict(
+                      id=processor.id,
+                      name=processor.name,
+                      storage_period=processor.storage_period,
+                      image_processors=processor.image_processors,
+                      create_date=processor.create_date,
+                      update_date=processor.update_date,
+                      status=processor.status,
+                      project=dict(
+                            id=processor.project.id,
+                            name=processor.project.name
+                            ),
+                      owner=dict(
+                            id=processor.owner.id,
+                            email=processor.owner.email
+                            ),
+                      cameras=[dict(id=camera.id, name=camera.name) for camera in processor.cameras],
+                      processor_operating=processor_operating
+                      )
+#         print("result:", result)
+        return result
+    
     @view_config(request_method='GET')
     def get(self):
         
