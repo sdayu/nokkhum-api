b9b86f79a49b2a9ad5a4b5f97a16e06e45df576c
Thanathip Limna <boatkrap@gmail.com>
2014-07-17 16:27:16 +0700

    fix for vm information

--- nokkhumapi/tests/projects/projects.py
+++ nokkhumapi/tests/projects/projects.py
@@ -12,27 +12,27 @@ class TestProjectAPI(unittest.TestCase):
 
     def setUp(self):
         from nokkhumapi import main
-        
+
         cfg = configparser.ConfigParser()
         cfg.read('../../../development.ini')
-        
+
         settings = dict(cfg.items('app:main'))
 
         app = main({}, **settings)
         from webtest import TestApp
         self.testapp = TestApp(app)
 
-        args = dict(password_credentials= {"email": "admin@nokkhum.local", 
+        args = dict(password_credentials={"email": "admin@nokkhum.local",
                                           "password": "password"}
                     )
         response = self.testapp.post_json('/authentication/tokens', params=args, status=200)
         print("authentication: ")
-        
-        self.pp=pprint.PrettyPrinter(indent=4)
+
+        self.pp = pprint.PrettyPrinter(indent=4)
         self.pp.pprint(response.json)
-        
+
         self.token = response.json['access']['token']['id']
-        
+
         self.project_id = 1
 
     def tearDown(self):
--- nokkhumapi/views/admin/compute_nodes.py
+++ nokkhumapi/views/admin/compute_nodes.py
@@ -5,24 +5,25 @@ from pyramid.security import authenticated_userid
 
 from nokkhumapi import models
 
+
 @view_defaults(route_name='admin.compute_nodes', permission='role:admin', renderer="json")
 class ComputeNode:
     def __init__(self, request):
         self.request = request
-        
+
     @view_config(route_name='admin.compute_nodes.list', permission='role:admin', renderer='json')
     def list_compute_node(self):
         compute_nodes = models.ComputeNode.objects().order_by("-updated_date").all()
         result = dict(
-                    compute_nodes = [ dict(
-                                           id=compute_node.id,
-                                           name=compute_node.name
-                                           )
-                                     for compute_node in compute_nodes]
-                    )
-        
+            compute_nodes=[dict(
+                id=compute_node.id,
+                name=compute_node.name
+                )
+                for compute_node in compute_nodes]
+            )
+
         return result
-        
+
     @view_config(request_method="GET")
     def show(self):
         matchdict = self.request.matchdict
@@ -33,70 +34,86 @@ class ComputeNode:
                                           host='Unavailable'
                                           ))
         return dict(
-                   compute_node=dict(
-                                     id = compute_node.id,
-                                     name = compute_node.name,
-                                     updated_date = compute_node.updated_date,
-                                     created_date = compute_node.created_date,
-                                     updated_resource_date = compute_node.updated_resource_date,
-                                     host = compute_node.host,
-                                     is_vm = compute_node.is_vm(),
-                                     online = compute_node.is_online(),
-                                     system = compute_node.system,
-                                     machine = compute_node.machine,
-                                     cpu = dict(
-                                              count=compute_node.cpu.count,
-                                              used=compute_node.cpu.used,
-                                              used_per_cpu=compute_node.cpu.used_per_cpu
-                                              ),
-                                     memory = dict(
-                                                total=compute_node.memory.total,
-                                                used=compute_node.memory.used,
-                                                free=compute_node.memory.free,
-                                                 ),
-                                     disk = dict(
-                                                total=compute_node.disk.total,
-                                                used=compute_node.disk.used,
-                                                free=compute_node.disk.free,
-                                                 ),
-                                     extra = compute_node.extra
-                                     )
-                    )
-    
+            compute_node=dict(
+                id=compute_node.id,
+                name=compute_node.name,
+                updated_date=compute_node.updated_date,
+                created_date=compute_node.created_date,
+                updated_resource_date=compute_node.updated_resource_date,
+                host=compute_node.host,
+                is_vm=compute_node.is_vm(),
+                online=compute_node.is_online(),
+                system=compute_node.system,
+                machine=compute_node.machine,
+                cpu=dict(
+                    count=compute_node.cpu.count,
+                    used=compute_node.cpu.used,
+                    used_per_cpu=compute_node.cpu.used_per_cpu
+                    ),
+                memory=dict(
+                    total=compute_node.memory.total,
+                    used=compute_node.memory.used,
+                    free=compute_node.memory.free,
+                    ),
+                disk=dict(
+                    total=compute_node.disk.total,
+                    used=compute_node.disk.used,
+                    free=compute_node.disk.free,
+                    ),
+                extra=compute_node.extra
+                )
+            )
+
     @view_config(request_method="DELETE")
     def delete(self):
         matchdict = self.request.matchdict
         compute_node_id = matchdict['compute_node_id']
-        
+
         compute_node = models.ComputeNode.objects().with_id(compute_node_id)
         compute_node.delete()
-        
-        
+
+    @view_config(route_name='admin.compute_nodes.processors',
+                 permission='admin',
+                 request_method='GET')
+    def get_processors(self):
+        matchdict = self.request.matchdict
+        compute_node_id = matchdict['compute_node_id']
+
+        compute_node = models.ComputeNode.objects().with_id(compute_node_id)
+        processors = models.Processor.objects(operating__compute_node=compute_node).all()
+
+        return dict(
+            processors=[dict(
+                id=processor.id,
+                name=processor.name
+            ) for processor in processors]
+        )
+
+
 @view_defaults(route_name='admin.compute_nodes.vm', permission='role:admin', renderer="json")
 class VM:
     def __init__(self, request):
         self.request = request
-        
+
     @view_config(request_method="GET")
     def show(self):
         matchdict = self.request.matchdict
         compute_node_id = matchdict['compute_node_id']
         compute_node = models.ComputeNode.objects().with_id(compute_node_id)
-        
+
         if not compute_node or compute_node.vm is None:
             return dict(vm=None)
         return dict(
-                   vm=dict(
-                            id=compute_node.id,
-                            name= compute_node.vm.name,
-                            image_id = compute_node.vm.image_id,
-                            instance_id= compute_node.vm.instance_id,
-                            instance_type= compute_node.vm.instance_type,
-                            ip_address= compute_node.vm.ip_address,
-                            private_ip_address= compute_node.vm.private_ip_address,
-                            started_instance_date= compute_node.vm.started_instance_date,
-                            status= compute_node.vm.status,
-                            extra = compute_node.vm.extra
-                                     
-                        )
-                    )
+            vm=dict(
+                id=compute_node.id,
+                name=compute_node.vm.name,
+                image_id=compute_node.vm.image_id,
+                instance_id=compute_node.vm.instance_id,
+                instance_type=compute_node.vm.instance_type,
+                ip_address=compute_node.vm.ip_address,
+                private_ip_address=compute_node.vm.private_ip_address,
+                started_instance_date=compute_node.vm.started_instance_date,
+                status=compute_node.vm.status,
+                extra=compute_node.vm.extra
+            )
+        )
