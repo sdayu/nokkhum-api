0a8c0aa1df2c1a26841f5150eff640c68d777481
Thanathip Limna <boatkrap@gmail.com>
2014-08-07 15:04:41 +0700

    add processor resource monitor

--- nokkhumapi/views/admin/processors.py
+++ nokkhumapi/views/admin/processors.py
@@ -5,18 +5,22 @@ Created on Sep 18, 2013
 '''
 from pyramid.view import view_defaults
 from pyramid.view import view_config
+from dateutil import tz
 
 from nokkhumapi import models
 
-@view_defaults(route_name='admin.processors', permission='role:admin', renderer='json')
+
+@view_defaults(route_name='admin.processors',
+               permission='role:admin',
+               renderer='json')
 class Processor:
     def __init__(self, request):
         self.request = request
-        
+
     def build_result(self, processor):
         if processor is None:
             return None
-        
+
         processor_operating=dict(
                              user_command=processor.operating.user_command,
                              status=processor.operating.status,
@@ -33,27 +37,27 @@ class Processor:
                 processor_operating['compute_node']['name'] = 'Unavailable'
 
         result = dict(
-                      id=processor.id,
-                      name=processor.name,
-                      storage_period=processor.storage_period,
-                      image_processors=processor.image_processors,
-                      created_date=processor.created_date,
-                      updated_date=processor.updated_date,
-                      status=processor.status,
-                      project=dict(
-                            id=processor.project.id,
-                            name=processor.project.name
-                            ),
-                      owner=dict(
-                            id=processor.owner.id,
-#                             email=processor.owner.email
-                            ),
-                      cameras=[dict(id=camera.id, name=camera.name) for camera in processor.cameras],
-                      processor_operating=processor_operating
-                      )
+            id=processor.id,
+            name=processor.name,
+            storage_period=processor.storage_period,
+            image_processors=processor.image_processors,
+            created_date=processor.created_date,
+            updated_date=processor.updated_date,
+            status=processor.status,
+            project=dict(
+                id=processor.project.id,
+                name=processor.project.name
+                ),
+            owner=dict(
+                id=processor.owner.id,
+                # email=processor.owner.email
+                ),
+            cameras=[dict(id=camera.id, name=camera.name) for camera in processor.cameras],
+            processor_operating=processor_operating
+            )
 #         print('result:', result)
         return result
-    
+
     @view_config(route_name='admin.processors.list', request_method='GET')
     def list_processor(self):
         if 'user_id' in self.request.GET:
@@ -61,22 +65,51 @@ class Processor:
             processors = models.Processor.objects(owner=user, status='active').order_by('-operating__updated_date').all()
         else:
             processors = models.Processor.objects(status='active').order_by('-operating__updated_date').all()
-        
+
         result = dict(
                 processors=[ self.build_result(processor)
                   for processor in processors
-                  ]   
+                  ]
                 )
-        
+
         return result
-    
+
     @view_config(request_method='GET')
     def get(self):
         processor_id = self.request.matchdict['processor_id']
         processor = models.Processor.objects().with_id(processor_id)
-        
+
         result = dict(
-                processor=self.build_result(processor)  
+                processor=self.build_result(processor)
                 )
-        
+
         return result
+
+    @view_config(route_name='admin.processors.resources',
+                 permission='admin',
+                 request_method='GET')
+    def get_resources(self):
+        matchdict = self.request.matchdict
+        processor_id = matchdict['processor_id']
+
+        processor = models.Processor.objects.with_id(processor_id)
+
+        processor_status = models.ProcessorStatus.objects(processor=processor)\
+            .order_by('-id')\
+            .limit(30)
+
+        ctz = tz.tzlocal()
+        resources = [
+            dict(
+                cpu=p.cpu,
+                memory=p.memory,
+                threads=p.threads,
+                reported_date=p.reported_date.replace(tzinfo=ctz)
+                ) for p in processor_status
+            ]
+
+        resources.reverse()
+
+        return dict(
+            resources=resources
+        )
