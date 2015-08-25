859a0274ed15f165e288bc0ce8d2dfe90f7ecf8c
Thanathip Limna <boatkrap@gmail.com>
2015-08-25 17:05:52 +0700

    change admin operation

--- nokkhumapi/views/admin/processor_operating.py
+++ nokkhumapi/views/admin/processor_operating.py
@@ -10,24 +10,24 @@ from pyramid.response import Response
 import json, datetime
 
 from nokkhumapi import models
-@view_defaults(route_name='admin.processors.operating', renderer="json", permission="authenticated")
-class CamearaOperating(object):
+@view_defaults(route_name='admin.processors.operating', renderer="json", permission="role:admin")
+class ProcessorOperating(object):
     def __init__(self, request):
         self.request = request
-    
+
     @view_config(request_method='POST')
     @view_config(request_method='PUT')
     def operate(self):
         matchdict   = self.request.matchdict
-        camera_id = matchdict['camera_id']
-    
-        camera = models.Camera.objects(owner=self.request.user, id=camera_id).first()
-        
-        if not camera:
+        processor_id = matchdict['processor_id']
+
+        processor = models.Processor.objects(id=processor_id).first()
+
+        if not processor:
             self.request.response.status = '404 Not Found'
-            return {'result':"camera not found id: %s"%camera_id}
-        
-        operating = self.request.json_body["camera_operating"]["action"]
+            return {'result':"camera not found id: %s"%processor_id}
+
+        operating = self.request.json_body["processor_operating"]["action"]
         command_action  = 'no-command'
         user_command    = 'undefined'
         if operating == 'start':
@@ -36,69 +36,81 @@ class CamearaOperating(object):
         elif operating == 'stop':
             command_action = 'stop'
             user_command = 'suspend'
-        
-        ccq = models.CameraCommandQueue.objects(owner=self.request.user, camera=camera, action=command_action).first()
-        if ccq is not None:
-            self.request.response.status = '406 Not Acceptable'
-            return {'result':'camera name %s on operation' % camera.id}
 
-        
-        camera.operating.status = command_action
-        camera.operating.user_command = user_command
-        camera.update_date = datetime.datetime.now()
-        camera.save()
-        
-        ccq         = models.CameraCommandQueue()
-        ccq.command_date = datetime.datetime.now()
-        ccq.update_date = datetime.datetime.now()
-        ccq.action  = command_action
-        ccq.status  = 'waiting'
-        ccq.camera  = camera
-        ccq.owner   = self.request.user
-        ccq.save()
-        print("update success")
+
+        pc = models.ProcessorCommand.objects(processor=processor, action=command_action, status__in=["waiting", "processing"]).first()
+        pcq = models.ProcessorCommandQueue.objects(processor_command=pc).first()
+        # pcq = models.ProcessorCommandQueue.objects(processor_command__owner=self.request.user, processor_command__processor=processor, processor_command__action=command_action).first()
+
+        if pcq is not None:
+            self.request.response.status = '406 Not Acceptable'
+            return {'result':'processor name %s on operation' % processor.id}
+
+
+#         camera.operating.status = command_action
+        processor.operating.user_command = user_command
+        processor.updated_date = datetime.datetime.now()
+        processor.save()
+
+        pc = models.ProcessorCommand()
+        pc.commanded_date = datetime.datetime.now()
+        pc.updated_date = datetime.datetime.now()
+        pc.action = command_action
+        pc.status = 'waiting'
+        pc.command_type = 'user'
+        pc.processor = processor
+        pc.owner = self.request.user
+        pc.save()
+
+        pcq         = models.ProcessorCommandQueue()
+        pcq.processor_command = pc
+        pcq.save()
+        pcq.reload()
+
+        processor.operating.user_command_log.append(pcq.processor_command.id)
+        processor.save()
+        processor.reload()
 
         return dict(
-                    camera_operating=dict(
-                           action=ccq.action,
-                           id=ccq.id,
-                           status=ccq.status,
-                           camera=dict(
-                                     id=ccq.camera.id                      
+                    processor_operating=dict(
+                           action=pc.action,
+                           id=pcq.id,
+                           status=pc.status,
+                           processor=dict(
+                                     id=pc.processor.id
                                      ),
                             user=dict(
-                                    id=ccq.owner.id
+                                    id=pc.owner.id
                                     )
                            ),
                     result="success"
                 )
-        
+
     @view_config(request_method='GET')
     def get(self):
         matchdict = self.request.matchdict
-        camera_id = matchdict.get('camera_id')
+        processor_id = matchdict.get('processor_id')
 
-        camera = models.Camera.objects(id=camera_id).first()
+        processor = models.Processor.objects(id=processor_id).first()
 
-        if not camera:
+        if not processor:
             self.request.response.status = '404 Not Found'
             return {}
         result = dict(
-                      camera_operating=dict(
-                            status=camera.operating.status, 
-                            update_date=camera.operating.update_date,
-                            user_command=camera.operating.user_command,
+                      processor_operating=dict(
+                            status=processor.operating.status,
+                            updated_date=processor.operating.updated_date,
+                            user_command=processor.operating.user_command,
+#                                         compute_node={'id':camera.operating.compute_node._id}
+
                             )
                       )
 
-        if camera.operating.compute_node is not None:
-            result["camera_operating"]["compute_node"] = {'id':camera.operating.compute_node.id} 
-        
         return result
-            
-        
-    
-    
-    
-    
-    
+
+
+
+
+
+
+
